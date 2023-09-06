4617de5af72d31d790ab13b860ae1440f1f8f3cd
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-06 10:18:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,21 @@ import soldout from './imges/soldout.jpg'
 import { AiOutlineApple } from "react-icons/ai";
 import './App.css'
 
-
 function App() {
 
   const refCaja = useRef();
   const cambio = 2;
 
   const incrementar = (e) => {
-    e.target.innerHTML=Number(e.target.innerHTML)+1;
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
   };
 
   const convertir = () => {
-    refCaja.current.innerHTML = Number(refCaja.current.innerHTML)*cambio;
-
+    refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * cambio;
   };
 
   const soldouts = (j) => {
-    j.target.src=soldout;
-
+    j.target.src = soldout;
   };
 
   const lectura = (e) => {
@@ -36,19 +33,19 @@ function App() {
       <div className='header-container'>
         <div className='container'>
           <h2><AiOutlineApple /> Apfel</h2>
-          <img onClick={soldouts} src={Imge0}/>
+          <img onClick={soldouts} src={Imge0} />
           <div className='valor' ref={refCaja} onClick={incrementar}>1</div>
           <button onClick={convertir}>Comprar</button>
           <input onChange={lectura} className='inp' type='number' placeholder='1234...' />
         </div>
         <div className='container'>
           <h2>Apfel</h2>
-          <img src={Imge1}/>
+          <img src={Imge1} />
           <button>Comprar</button>
         </div>
         <div className='container'>
           <h2>Apfel</h2>
-          <img src={Imge2}/>
+          <img src={Imge2} />
           <button>Comprar</button>
         </div>
       </div>
