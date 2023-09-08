589b522da63f6545c80397d10e254eb56d924419
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-08 12:46:32 +0200

    update
    api

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # React + Vite
 
 Learning React -Test site...
+Cards 
+Btns
+Api Test -randomuser
+IMg
 ![Alt text](image.png)
--- src/components/Fetsch.jsx
+++ src/components/Fetsch.jsx
@@ -2,32 +2,35 @@ import React, { useEffect, useState } from 'react'
 import '../stylesheet/fetsch.css'
 
 const Fetsch = () => {
-    const [nombre, setNombre] = useState("")
-    const [imagen, setImagen] = useState("")
-    useEffect(() => {
-        //here we do peticion asincrona
-        const url = "https://randomuser.me/api/"
-        const peticion = fetch(url);
+    const [datos, setDatos] = useState([])
 
+
+    useEffect(() => {
+        const url = "https://randomuser.me/api/?results=2"
+        const peticion = fetch(url);
         peticion
             .then(datos => datos.json())
             .then(lectura => {
-                setNombre(`${lectura.results[0].name.first} ${lectura.results[0].name.last}`)
-                setImagen(`${lectura.results[0].picture.large}`)
+                lectura.results.map((persona) => {
+                    setDatos((e) =>
+                        [...e, <div key={ persona.email }>
+                            <div>
+                                <img src={persona.picture.large} />
+                            </div>
+                            <div>{ persona.name.first } { persona.name.last } </div>
+                        </div>])
+                })
             })
-            .catch(() => console.log("error Diego"))
+            .catch(console.log())
     }, [])
-
-
 
     return (
         <>
             <div className='fetsch-header'>
                 <h2>Random People Refresh</h2>
-                {nombre}
-                <div className='imgsection'>
-                    <img src={imagen} />
-                </div>
+
+
+                {datos}
             </div>
 
 
